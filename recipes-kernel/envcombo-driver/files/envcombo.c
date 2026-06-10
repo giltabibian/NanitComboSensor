40b890826680b911320f028d9d537673accc5fc1
Gil Tabibian <giltabibian@gmail.com>
2026-06-10 14:48:05 +0300

    als gain & time attributes

--- recipes-kernel/envcombo-driver/files/envcombo.c
+++ recipes-kernel/envcombo-driver/files/envcombo.c
@@ -11,6 +11,7 @@
 #include <linux/completion.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
+#include <linux/jiffies.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/regmap.h>
@@ -22,6 +23,8 @@
 #define ENVCOMBO_REG_CFG	0x06
 #define ENVCOMBO_REG_INT_CFG	0x07
 #define ENVCOMBO_REG_STATUS	0x0C
+#define ENVCOMBO_REG_CAL_ALS_GAIN	0x10
+#define ENVCOMBO_REG_CAL_ALS_TIME	0x11
 #define ENVCOMBO_REG_PWR_MODE	0x12
 
 #define ENVCOMBO_WHO_AM_I_VAL	0xEB
@@ -46,6 +49,19 @@
 #define ENVCOMBO_DEFAULT_GAIN_IDX	0
 #define ENVCOMBO_DEFAULT_TIME_IDX	2
 
+static const unsigned int envcombo_als_gain_table[] = { 1, 4, 16, 64 };
+static const unsigned int envcombo_als_time_table_us[] = {
+	50000, 100000, 200000, 400000,
+};
+
+/* IIO_VAL_INT_PLUS_MICRO available list: (integer, micro) pairs */
+static const int envcombo_als_time_avail[] = {
+	0, 50000,
+	0, 100000,
+	0, 200000,
+	0, 400000,
+};
+
 struct envcombo_data {
 	struct regmap *regmap;
 	struct mutex lock;
@@ -53,6 +69,8 @@ struct envcombo_data {
 
 	u8 als_gain_idx;
 	u8 als_time_idx;
+	u8 calib_again;
+	u8 calib_atime;
 
 	bool ev_en_rising;
 	bool ev_en_falling;
@@ -62,7 +80,13 @@ struct envcombo_data {
 static const struct iio_chan_spec envcombo_channels[] = {
 	{
 		.type = IIO_LIGHT,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW) |
+				       BIT(IIO_CHAN_INFO_SCALE) |
+				       BIT(IIO_CHAN_INFO_HARDWAREGAIN) |
+				       BIT(IIO_CHAN_INFO_INT_TIME),
+		.info_mask_separate_available =
+				       BIT(IIO_CHAN_INFO_HARDWAREGAIN) |
+				       BIT(IIO_CHAN_INFO_INT_TIME),
 	},
 };
 
@@ -167,6 +191,8 @@ static int envcombo_probe(struct i2c_client *client)
 		return -ENOMEM;
 
 	data = iio_priv(indio_dev);
+	mutex_init(&data->lock);
+	init_completion(&data->als_done);
 
 	data->regmap = devm_regmap_init_i2c(client, &envcombo_regmap_config);
 	if (IS_ERR(data->regmap))
@@ -183,6 +209,28 @@ static int envcombo_probe(struct i2c_client *client)
 	indio_dev->info = &envcombo_info;
 	indio_dev->channels = envcombo_channels;
 	indio_dev->num_channels = ARRAY_SIZE(envcombo_channels);
+
+	data->als_gain_idx = ENVCOMBO_DEFAULT_GAIN_IDX;
+	data->als_time_idx = ENVCOMBO_DEFAULT_TIME_IDX;
+
+	ret = regmap_write(data->regmap, ENVCOMBO_REG_CFG,
+			    ENVCOMBO_CFG_ALS_EN |
+			    FIELD_PREP(ENVCOMBO_CFG_ALS_GAIN_MASK, data->als_gain_idx) |
+			    FIELD_PREP(ENVCOMBO_CFG_ALS_TIME_MASK, data->als_time_idx));
+	if (ret)
+		return ret;
+
+	ret = regmap_write(data->regmap, ENVCOMBO_REG_INT_CFG,
+			    ENVCOMBO_INT_CFG_EN | ENVCOMBO_INT_CFG_LATCH);
+	if (ret)
+		return ret;
+
+	ret = devm_request_threaded_irq(dev, client->irq, NULL,
+					 envcombo_irq_thread,
+					 IRQF_TRIGGER_LOW | IRQF_ONESHOT,
+					 "envcombo", indio_dev);
+	if (ret)
+		return ret;
 
 	return devm_iio_device_register(dev, indio_dev);
 }
