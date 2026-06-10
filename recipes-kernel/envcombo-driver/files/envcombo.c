0a21ef26e8c476400c8a4629381ab2eb808e314e
Gil Tabibian <giltabibian@gmail.com>
2026-06-10 14:43:18 +0300

    power moding

--- recipes-kernel/envcombo-driver/files/envcombo.c
+++ recipes-kernel/envcombo-driver/files/envcombo.c
@@ -6,20 +6,57 @@
  * the temperature and humidity channels are out of scope for this driver.
  */
 
+#include <linux/bitfield.h>
+#include <linux/bits.h>
+#include <linux/completion.h>
 #include <linux/i2c.h>
+#include <linux/interrupt.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/regmap.h>
 
 #include <linux/iio/iio.h>
 
 #define ENVCOMBO_REG_WHO_AM_I	0x00
+#define ENVCOMBO_REG_ALS_MSB	0x04
+#define ENVCOMBO_REG_CFG	0x06
+#define ENVCOMBO_REG_INT_CFG	0x07
+#define ENVCOMBO_REG_STATUS	0x0C
 #define ENVCOMBO_REG_PWR_MODE	0x12
-#define ENVCOMBO_MAX_REG	ENVCOMBO_REG_PWR_MODE
 
 #define ENVCOMBO_WHO_AM_I_VAL	0xEB
 
+#define ENVCOMBO_CFG_ALS_EN		BIT(7)
+#define ENVCOMBO_CFG_ALS_GAIN_MASK	GENMASK(4, 3)
+#define ENVCOMBO_CFG_ALS_TIME_MASK	GENMASK(2, 1)
+
+#define ENVCOMBO_INT_CFG_EN	BIT(7)
+#define ENVCOMBO_INT_CFG_LATCH	BIT(6)
+
+#define ENVCOMBO_STATUS_ALS_RDY	BIT(3)
+
+#define ENVCOMBO_PWR_MODE_MASK	GENMASK(1, 0)
+#define ENVCOMBO_PWR_SLEEP	0x01
+#define ENVCOMBO_PWR_ONE_SHOT	0x02
+#define ENVCOMBO_PWR_CONTINUOUS	0x03
+
+#define ENVCOMBO_RAW_READ_TIMEOUT_MS	500
+
+/* Defaults written to CFG[4:3]/[2:1] at probe: gain x1, integration time 200ms */
+#define ENVCOMBO_DEFAULT_GAIN_IDX	0
+#define ENVCOMBO_DEFAULT_TIME_IDX	2
+
 struct envcombo_data {
 	struct regmap *regmap;
+	struct mutex lock;
+	struct completion als_done;
+
+	u8 als_gain_idx;
+	u8 als_time_idx;
+
+	bool ev_en_rising;
+	bool ev_en_falling;
+	bool buffer_en;
 };
 
 static const struct iio_chan_spec envcombo_channels[] = {
@@ -29,21 +66,91 @@ static const struct iio_chan_spec envcombo_channels[] = {
 	},
 };
 
+/* Caller must hold data->lock. */
+static int envcombo_update_power_mode(struct envcombo_data *data)
+{
+	bool active = data->buffer_en || data->ev_en_rising ||
+		      data->ev_en_falling;
+
+	return regmap_update_bits(data->regmap, ENVCOMBO_REG_PWR_MODE,
+				   ENVCOMBO_PWR_MODE_MASK,
+				   active ? ENVCOMBO_PWR_CONTINUOUS :
+					    ENVCOMBO_PWR_SLEEP);
+}
+
+static int envcombo_read_als_raw(struct envcombo_data *data, int *val)
+{
+	u8 buf[2];
+	int ret;
+
+	mutex_lock(&data->lock);
+
+	if (!data->buffer_en && !data->ev_en_rising && !data->ev_en_falling) {
+		reinit_completion(&data->als_done);
+
+		ret = regmap_write(data->regmap, ENVCOMBO_REG_PWR_MODE,
+				    ENVCOMBO_PWR_ONE_SHOT);
+		if (ret)
+			goto out_unlock;
+
+		if (!wait_for_completion_timeout(&data->als_done,
+				msecs_to_jiffies(ENVCOMBO_RAW_READ_TIMEOUT_MS))) {
+			ret = -ETIMEDOUT;
+			goto out_unlock;
+		}
+	}
+
+	ret = regmap_bulk_read(data->regmap, ENVCOMBO_REG_ALS_MSB, buf, 2);
+	if (ret)
+		goto out_unlock;
+
+	*val = ((int)buf[0] << 8) | buf[1];
+	ret = 0;
+
+out_unlock:
+	mutex_unlock(&data->lock);
+	return ret;
+}
+
 static int envcombo_read_raw(struct iio_dev *indio_dev,
 			      struct iio_chan_spec const *chan,
 			      int *val, int *val2, long mask)
 {
-	return -EOPNOTSUPP;
+	struct envcombo_data *data = iio_priv(indio_dev);
+
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		return envcombo_read_als_raw(data, val) ?: IIO_VAL_INT;
+	default:
+		return -EINVAL;
+	}
 }
 
 static const struct iio_info envcombo_info = {
 	.read_raw = envcombo_read_raw,
 };
 
+static irqreturn_t envcombo_irq_thread(int irq, void *private)
+{
+	struct iio_dev *indio_dev = private;
+	struct envcombo_data *data = iio_priv(indio_dev);
+	unsigned int status;
+	int ret;
+
+	ret = regmap_read(data->regmap, ENVCOMBO_REG_STATUS, &status);
+	if (ret)
+		return IRQ_NONE;
+
+	if (status & ENVCOMBO_STATUS_ALS_RDY)
+		complete(&data->als_done);
+
+	return IRQ_HANDLED;
+}
+
 static const struct regmap_config envcombo_regmap_config = {
 	.reg_bits = 8,
 	.val_bits = 8,
-	.max_register = ENVCOMBO_MAX_REG,
+	.max_register = ENVCOMBO_REG_PWR_MODE,
 	.cache_type = REGCACHE_NONE,
 };
 
