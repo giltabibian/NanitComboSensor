b13f25e7ecb01e2a3646a70dcd540d271cbff35d
Gil Tabibian <giltabibian@gmail.com>
2026-06-11 11:37:23 +0300

    reorder macros

--- recipes-kernel/envcombo-driver/files/envcombo.c
+++ recipes-kernel/envcombo-driver/files/envcombo.c
@@ -22,6 +22,7 @@
 #include <linux/iio/trigger_consumer.h>
 #include <linux/iio/triggered_buffer.h>
 
+#define ENVCOMBO_WHO_AM_I_VAL			0xEB
 #define ENVCOMBO_REG_WHO_AM_I			0x00
 #define ENVCOMBO_REG_ALS_MSB			0x04
 #define ENVCOMBO_REG_CFG				0x06
@@ -33,17 +34,15 @@
 #define ENVCOMBO_REG_CAL_ALS_TIME		0x11
 #define ENVCOMBO_REG_PWR_MODE			0x12
 
-#define ENVCOMBO_WHO_AM_I_VAL			0xEB
-
 #define ENVCOMBO_PWR_SLEEP				0x01
 #define ENVCOMBO_PWR_ONE_SHOT			0x02
 #define ENVCOMBO_PWR_CONTINUOUS			0x03
 
-#define ENVCOMBO_CFG_ALS_EN				BIT(7)
-#define ENVCOMBO_INT_CFG_EN				BIT(7)
-#define ENVCOMBO_INT_CFG_LATCH			BIT(6)
 #define ENVCOMBO_STATUS_ALS_INT			BIT(0)
 #define ENVCOMBO_STATUS_ALS_RDY			BIT(3)
+#define ENVCOMBO_INT_CFG_LATCH			BIT(6)
+#define ENVCOMBO_INT_CFG_EN				BIT(7)
+#define ENVCOMBO_CFG_ALS_EN				BIT(7)
 
 #define ENVCOMBO_CFG_ALS_GAIN_MASK		GENMASK(4, 3)
 #define ENVCOMBO_CFG_ALS_TIME_MASK		GENMASK(2, 1)
@@ -53,7 +52,6 @@
 #define ENVCOMBO_DEFAULT_GAIN_IDX		0
 #define ENVCOMBO_DEFAULT_TIME_IDX		2
 #define ENVCOMBO_RAW_READ_TIMEOUT_MS	500
-
 
 static const int envcombo_als_gain_table[] = { 1, 4, 16, 64 };
 static const int envcombo_als_time_table_us[] = {
